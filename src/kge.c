0b8d384d06a0948054f7d7c94be3d944e3b8beb2
Kendall Schmit <kendall504@me.com>
2022-02-14 14:59:44 -0800

    Clutter kge.c with histogram stuff

--- src/kge.c
+++ src/kge.c
@@ -17,6 +17,80 @@
 #include "vao.h"
 #include "draw.h"
 
+#define WHITE           ((struct vec3){ 1.0, 1.0, 1.0 })
+#define GRAY            ((struct vec3){ 0.5, 0.5, 0.5 })
+#define RED             ((struct vec3){ 1.0, 0.0, 0.0 })
+#define GREEN           ((struct vec3){ 0.0, 1.0, 0.0 })
+#define BLUE            ((struct vec3){ 0.0, 0.0, 1.0 })
+#define LIGHT_BLUE      ((struct vec3){ 0.2, 0.5, 1.0 })
+#define CYAN            ((struct vec3){ 0.0, 1.0, 1.0 })
+#define MAGENTA         ((struct vec3){ 1.0, 0.0, 1.0 })
+#define YELLOW          ((struct vec3){ 1.0, 1.0, 0.0 })
+#define ORANGE          ((struct vec3){ 1.0, 0.5, 0.0 })
+
+#define HIST_SAMPLES (120)
+#define HIST_WIDTH (0.5)
+#define HIST_SCALE_X (HIST_WIDTH / HIST_SAMPLES)
+#define HIST_SAMPLE_WIDTH (HIST_WIDTH / HIST_SAMPLES)
+
+#define HIST_HEIGHT (1.0)
+#define HIST_MAX_VALUE (4.0 / 60.0)
+#define HIST_SCALE_Y (HIST_HEIGHT / HIST_MAX_VALUE)
+
+#define HIST_OFFSET_X (-1.0)
+#define HIST_OFFSET_Y (0.0)
+
+#define HIST_SCALE ((struct vec2){ HIST_SCALE_X, HIST_SCALE_Y })
+#define HIST_OFFSET ((struct vec2){ HIST_OFFSET_X, HIST_OFFSET_Y })
+
+#define HIST_REF_OFFSET_X (HIST_OFFSET_X)
+#define HIST_REF_SCALE_X (HIST_WIDTH)
+
+struct histogram {
+    GLfloat max_value;
+
+    size_t index;
+    GLfloat values[HIST_SAMPLES + 1];
+};
+
+static void histogram_append(struct histogram *h, GLfloat v)
+{
+    h->values[h->index] = v;
+    h->index++;
+    if (h->index >= ARRAY_LEN(h->values)) {
+        h->index = 0;
+        h->values[h->index] = v;
+        h->index++;
+    }
+}
+
+static void histogram_draw(
+        struct histogram *h,
+        GLfloat width,
+        struct vec3 color)
+{
+    struct vec2 offset = HIST_OFFSET;
+
+    draw_lines(offset, HIST_SCALE, width, color, &h->values[0], h->index);
+
+    offset.x += HIST_SAMPLE_WIDTH * h->index;
+    draw_lines(offset, HIST_SCALE, width, color,
+            &h->values[h->index], ARRAY_LEN(h->values) - h->index);
+}
+
+static void histogram_reference(GLfloat value, struct vec3 color) {
+    GLfloat values[] = { value, value };
+    draw_lines((struct vec2){ HIST_REF_OFFSET_X, HIST_OFFSET_Y },
+            (struct vec2){ HIST_REF_SCALE_X, HIST_SCALE_Y },
+            1,
+            color,
+            values, ARRAY_LEN(values));
+}
+
+static struct histogram dt_histogram = { 0 };
+static struct histogram dt_smooth_histogram = { 0 };
+static struct histogram dt_median_histogram = { 0 };
+
 static void error_callback(int error, const char* description);
 static void window_size_callback(GLFWwindow *glfw_window, int x, int y);
 
@@ -107,27 +181,38 @@ void kge_deinit(void)
 
 uint64_t kge_show_frame(void)
 {
+    histogram_reference(0.0, WHITE);
+    histogram_reference(1.0 / 60.0, GRAY);
+    histogram_reference(2.0 / 60.0, WHITE);
+    histogram_draw(&dt_median_histogram, 2, LIGHT_BLUE);
+    histogram_draw(&dt_histogram, 2, RED);
+    histogram_draw(&dt_smooth_histogram, 2, GREEN);
+
     glfwSwapBuffers(window);
 
     uint64_t dt = kge_timer_now() - current_time;
     current_time += dt;
 
-    uint64_t dt_median = median_dt_hist();
     uint64_t dt_smooth = 0;
     if (current_time > current_time_smooth)
         dt_smooth = current_time - current_time_smooth;
 
+    uint64_t dt_median = median_dt_hist();
     if (dt_median != 0) {
         uint64_t nframes = (dt_smooth + dt_median / 2) / dt_median;
-        if (nframes < 1)
+        if (nframes == 0)
             nframes = 1;
-        dt_smooth = (dt_smooth * 1 + dt_median * nframes * 2) / 3;
+        dt_smooth = (dt_smooth + 5 * dt_median * nframes) / 6;
         append_dt_hist(dt / nframes);
     }
     else {
         append_dt_hist(dt);
     }
     current_time_smooth += dt_smooth;
+
+    histogram_append(&dt_histogram, (GLfloat)dt / 1000000000);
+    histogram_append(&dt_smooth_histogram, (GLfloat)dt_smooth / 1000000000);
+    histogram_append(&dt_median_histogram, (GLfloat)dt_median / 1000000000);
 
     return dt_smooth;
 }
